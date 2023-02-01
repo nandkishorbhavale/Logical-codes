70cc6e93d2612047496b2143bb08b5062128c837
Nandkishor Bhavale <nandkishor.bhavale@gmail.com>
2023-02-01 15:12:48 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args) {
+        // int arr[]=new int[5];
+        int arr[] = { 10, 45, 34, 89, 43 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
