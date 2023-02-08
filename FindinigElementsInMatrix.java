1d95971628de4246b607acb5e46b93c1d21f4e08
Nandkishor Bhavale <nandkishor.bhavale@gmail.com>
2023-02-08 17:56:00 +0530

    Add files via upload

---
+++ FindinigElementsInMatrix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FindinigElementsInMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows: ");
+        int rows = sc.nextInt();
+        System.out.println("Enter the cols: ");
+        int cols = sc.nextInt();
+        System.out.println("Enter the element of the matrix: ");
+        int[][] matrix = new int[rows][cols];
+        // storing arrys in matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter the matrix element you want to find it's index: ");
+        int find = sc.nextInt();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == find) {
+                    System.out.println(find + " This elements occures at the index of: " + i + "," + j);
+                }
+            }
+        }
+
+    }
+}
