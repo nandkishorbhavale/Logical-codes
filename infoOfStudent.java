64e1d6bee7c3244b5a088b7ad61513cdcf4f316e
Nandkishor Bhavale <nandkishor.bhavale@gmail.com>
2023-02-16 19:24:22 +0530

    Add files via upload

---
+++ infoOfStudent.java
@@ -0,0 +1,25 @@
+class student {
+    String name;
+    int age;
+    float marks;
+
+    public void displayInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.marks);
+    }
+
+    student(String name, int age, float marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+}
+
+public class infoOfStudent {
+    public static void main(String[] args) {
+        student std1 = new student("Nandkishor Jadhav", 23, 87.90f);
+        std1.displayInfo();
+        System.out.println(std1.marks);
+    }
+}
