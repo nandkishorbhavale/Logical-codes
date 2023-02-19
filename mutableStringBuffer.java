cde467e3a512de6bf0b479a9fa9e160c7e388448
Nandkishor Bhavale <nandkishor.bhavale@gmail.com>
2023-02-19 13:55:51 +0530

    Add files via upload

---
+++ mutableStringBuffer.java
@@ -0,0 +1,8 @@
+public class mutableStringBuffer {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Nandkishor");
+        sb.append(" bhavale");
+        System.out.println(sb);
+
+    }
+}
