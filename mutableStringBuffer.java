6da0dfb92ba026b4c8618fbe185f48e2564464c7
Nandkishor Bhavale <nandkishor.bhavale@gmail.com>
2023-04-16 21:53:03 +0530

    Update mutableStringBuffer.java

--- mutableStringBuffer.java
+++ mutableStringBuffer.java
@@ -1,6 +1,6 @@
 public class mutableStringBuffer {   // Mutable String Buffer code
     public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer("Nandkishor");
+        StringBuffer sb = new StringBuffer("Nandkishor"); //String Buffer Object
         sb.append(" bhavale");
         System.out.println(sb);
 
